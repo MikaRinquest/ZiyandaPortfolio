af04ca355acb21808715fe8005db966170b272cf
MikaRinquest <rinquestmika@gmail.com>
2024-07-18 23:40:49 +0200

    Added route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <!-- Turning whatever route we are in into a component -->
+
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+      <!-- Using whatever route we are currently in as a component-->
+    </transition>
+  </router-view>
 </template>
 
 <style>
@@ -33,5 +40,28 @@ a {
 .triangle {
   height: 1rem;
   aspect-ratio: 1;
+}
+
+/* Route transitions */
+
+/* The below runs the beginning of the animation(entering) */
+.route-enter-from {
+  /* Where the transition is coming from at the start of the animation */
+  opacity: 0; /* When starting the transition from one route to the next, everything will 'vanish' */
+  transform: translateX(-1000px); /* The animation will come in from the left */
+}
+
+/* The below dictates what happens during the animation, usually just determining how fast the transition will be */
+.route-enter-active,
+.route-leave-active {
+  /* Does not need to have the same speed */
+  transition: all 0.3s ease-out;
+  /* You do not need to specify opacity:1; because the animation automatically goes to opacity:1; */
+}
+
+/* The below runs the end of the animation(exiting) */
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(1000px); /* The animation will go to the right */
 }
 </style>
